26e9be7e5305130f798be2ea42f73311120b0ea6
bgroveben <bgroveben@gmail.com>
2021-05-02 18:08:30 -0700

    Modify boilerplate CSS stylesheet and finish Survey Form project for Responsive Web Design Projects

--- scientific_computing_with_python/examples.py
+++ scientific_computing_with_python/examples.py
@@ -1,16 +1,16 @@
-smallest = None
-print("Before:", smallest)
-for itervar in [3, 41, 12, 9, 74, 15]:
-    if smallest is None or itervar < smallest:
-        smallest = itervar
-        break
-    print("Loop:", itervar, smallest)
-print("Smallest:", smallest)
+import json
 
-words = 'His e-mail is q-lar@freecodecamp.org'
-pieces = words.split()
-parts = pieces[3].split('-')
-n = parts[1]
-print(pieces)
-print(parts)
-print(n)
+data = '''
+    [
+        { "id" : "001",
+          "x" : "2",
+          "name" : "Quincy"
+        },
+        {  "id" : "009",
+            "x" : "7",
+            "name" : "Mrugesh"
+        }
+    ]
+'''
+info = json.loads(data)
+print(info[1]['name'])
